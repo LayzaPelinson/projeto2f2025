6225bfc742399e65f9f04f71bc3f9e03b6ea285f
LayzaPelinson <rm95322@estudante.fieb.edu.br>
2025-03-14 09:38:39 -0300

    projeto2f

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+class App { Component } from "react";
+
+class App extends Component{
+  render(){
+
+    return (
+      <div>
+        <h3>Aula React Vite</h3>
+        <p>Retomada do Conteúdo</p>
+      </div>
+    )
+  }
+}
+
+
+export default App
